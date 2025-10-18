9c4d1db3b50722b66b66ae5707fe9871cee9f9e0
Lorena πacente <149822592+LorenaPiacente@users.noreply.github.com>
2025-10-18 19:52:08 +0000

    Atualiza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Chuchu, eu te amo muito! Se me amas, me manda um beijinho!</h1>
+    return <h1>Chuchu, eu te amo muito! Se me amas, me manda um beijinho! S2</h1>
 }
 
 export default Home;
