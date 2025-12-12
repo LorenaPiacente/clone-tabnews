8815eea1cebce87e8e649a3a58eb7a2b16dc46a6
Lorena πacente <149822592+LorenaPiacente@users.noreply.github.com>
2025-12-12 18:47:23 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  // console.log(database);
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   // response.status(200).send("Status 200");
   response.status(200).json({ chave: "Testando o UTF-8 charset com ç e `^^`" });
 }
